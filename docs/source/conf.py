50dd6280dcf384f401c9feff68f0c5fe132e9493
YifZhou <zhouyifan1012@gmail.com>
2019-01-03 16:26:48 -0700

    remove nbsphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
